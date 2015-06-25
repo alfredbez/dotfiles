c2fb6dbc5f888861769b076e5e3ff588ca6bebb2
Alfred Bez <alfred.bez@googlemail.com>
2015-06-25 10:59:08 +0200

    updated scripts

--- scripts/createCommitMsgHook.sh
+++ scripts/createCommitMsgHook.sh
@@ -2,18 +2,23 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [ ! -d $dir/.git ]; then
     echo "isn't a git repository"
 else
-    echo "Bitte gib das JIRA Projekt-Kürzel an:"
-    read jirakey
-    if [ -z "$jirakey" ]; then
-        echo "leeres JIRA-Kürzel!"
+    if [ -f $dir/.git/hooks/commit-msg ]; then
+        echo "Es gibt bereits einen commit-msg hook, hier mal die erste Zeile:"
+        head -n 1 $dir/.git/hooks/commit-msg
     else
-        cat <<EOF >$dir/.git/hooks/commit-msg
+        echo "Bitte gib das JIRA Projekt-Kürzel an:"
+        read jirakey
+        if [ -z "$jirakey" ]; then
+            echo "leeres JIRA-Kürzel!"
+        else
+            cat <<EOF >$dir/.git/hooks/commit-msg
 test "" != "\$(grep '$jirakey-' "\$1")" || {
-    echo >&2 "ERROR: Commit message is missing Jira issue number."
+    echo >&2 "ERROR: Commit message is missing Jira issue number ('$jirakey-')."
     exit 1
 }
 EOF
-        chmod a+x $dir/.git/hooks/commit-msg
-        echo "commit-msg hook mit dem JIRA-Kürzel $jirakey wurde erfolgreich erstellt"
+            chmod a+x $dir/.git/hooks/commit-msg
+            echo "commit-msg hook mit dem JIRA-Kürzel $jirakey wurde erfolgreich erstellt"
+        fi
     fi
 fi
---
+++ scripts/setBitbucketUser.sh
@@ -0,0 +1,2 @@
+git config user.email a.bez@bkm-medien.de
+git config user.name "Alfred Bez"
