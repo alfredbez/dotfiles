8bfa8e909b01a3c502d613f88d14c780ebe6a1a4
Alfred Bez <alfred.bez@googlemail.com>
2015-09-02 17:15:07 +0200

    created auto installer

--- README.md
+++ README.md
@@ -1,36 +1,43 @@
 # Installation
 
-1. Clone this repo: `cd ~ && git clone git@github.com:alfredbez/dotfiles.git`
-
-1. `cd` into the repo: `cd dotfiles`
+1. Clone this repo: `git clone git@github.com:alfredbez/dotfiles.git '~/.dotfiles'`
 
 1. symlink files to your home-directory
 
+# Automatic installation
+
+Just run `. ~/.dotfiles/install.sh`
+
+# Manual installation
+
 ##VIM
-`ln -s dotfiles/.vim ~/.vim && ln -s dotfiles/.vimrc ~/.vimrc`
+`ln -s ~/.dotfiles/.vim ~/.vim && ln -s ~/.dotfiles/.vimrc ~/.vimrc`
 
 ##ZSH
 Install [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh) first.
 Then symlink `.zshrc` to your home-directory:
 ````
-ln -s dotfiles/.zshrc ~/.zshrc
-ln -s dotfiles/oh-my-zsh-custom ~/.oh-my-zsh/custom
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/oh-my-zsh-custom ~/.oh-my-zsh/custom
 ````
 
 ##global .gitignore
 ````
-ln -s dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 ````
 
 ##other
 
 ````
-ln -s ~/dotfiles/.ackrc ~/.ackrc
-ln -s ~/dotfiles/.curlrc ~/.curlrc
-ln -s ~/dotfiles/.editorconfig ~/.editorconfig
-ln -s ~/dotfiles/.wgetrc ~/.wgetrc
+ln -s ~/.dotfiles/.ackrc ~/.ackrc
+ln -s ~/.dotfiles/.curlrc ~/.curlrc
+ln -s ~/.dotfiles/.editorconfig ~/.editorconfig
+ln -s ~/.dotfiles/.wgetrc ~/.wgetrc
 # on OS X
-ln -s ~/dotfiles/sublime-text/Preferences.sublime-settings ~/Library/Application Support/Sublime Text 3/Packages/User/
-ln -s ~/dotfiles/sublime-text/Default (OSX).sublime-keymap ~/Library/Application Support/Sublime Text 3/Packages/User/
+ln -s ~/.dotfiles/sublime-text/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+ln -s ~/.dotfiles/sublime-text/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+# on Linux
+ln -s ~/.dotfiles/sublime-text/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
+ln -s ~/.dotfiles/sublime-text/Default\ \(Linux\).sublime-keymap ~/.config/sublime-text-3/Packages/User/
 ````
---
+++ install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# some helper functions, credits to Ben "cowboy" Alman
+# see https://github.com/cowboy/dotfiles/blob/master/bin/dotfiles#L26-L30
+function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
+function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
+function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
+function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
+
+function symlink() {
+	if [ -h "$2" ]; then
+		local target="$(readlink -f ${2})"
+		if [ "$1" == "$target" ]; then
+			# don't create symlink if it exists and the target is the same
+			return 0
+		fi
+	fi
+	if [ -f "$2" ]; then
+    e_error "File already exists!"
+    local newname="$2.$(date +%s)"
+    mv "$2" "$newname" && e_success "renamed to $newname"
+	fi
+	ln -s "$1" "$2"
+}
+
+# zsh
+if [ ! -d $HOME/.oh-my-zsh ]; then
+	e_error "oh-my-zsh not found!"
+	e_header "install it automatically..."
+	git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
+	if [ -d $HOME/.oh-my-zsh ]; then
+		e_success "installed oh-my-zsh"
+	fi
+fi
+
+if [ -d $HOME/.oh-my-zsh ]; then
+	symlink "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
+	rm -rf "$HOME/.oh-my-zsh/custom"
+	symlink "$HOME/.dotfiles/oh-my-zsh-custom" "$HOME/.oh-my-zsh/custom"
+	e_success "created zsh symlinks"
+else
+	e_error "something gone wrong, you need to install oh-my-zsh on manually"
+fi
+
+# VIM stuff
+symlink "$HOME/.dotfiles/.vim" "$HOME/.vim"
+symlink "$HOME/.dotfiles/.vimrc" "$HOME/.vimrc"
+e_success "created vim symlinks"
+
+# git
+symlink "$HOME/.dotfiles/.gitignore_global" "$HOME/.gitignore_global"
+git config --global core.excludesfile $HOME/.gitignore_global
+e_success "configured git"
+e_arrow "reminder: set your git name and email like this: \
+\n\t git config --global user.email \"foo@bar.com\" \
+\n\t git config --global user.name \"Foo Bar\""
+
+# sublime-text
+if [ "$(uname)" == "Darwin" ]; then
+  # OS X
+  symlink "$HOME/.dotfiles/sublime-text/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+  symlink "$HOME/.dotfiles/sublime-text/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
+  e_success "configured sublime text (OSX)"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	# Linux
+  symlink "$HOME/.dotfiles/sublime-text/Preferences.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
+  symlink "$HOME/.dotfiles/sublime-text/Default (Linux).sublime-keymap" "$HOME/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
+  e_success "configured sublime text (Linux)"
+fi
+
+# random
+symlink "$HOME/.dotfiles/.ackrc" "$HOME/.ackrc"
+symlink "$HOME/.dotfiles/.curlrc" "$HOME/.curlrc"
+symlink "$HOME/.dotfiles/.editorconfig" "$HOME/.editorconfig"
+symlink "$HOME/.dotfiles/.wgetrc" "$HOME/.wgetrc"
+e_success "symlinked random stuff"
