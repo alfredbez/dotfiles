0392da49dcca0e825b545b631827356f513cde3d
alfredbez <alfred.bez@googlemail.com>
2019-05-27 12:26:34 +0300

    cleaned zsh-aliases

--- oh-my-zsh-custom/20_aliases.zsh
+++ oh-my-zsh-custom/20_aliases.zsh
@@ -1,16 +1,10 @@
 alias rs='rsync -chavzP --stats --exclude=.git --exclude=.svn'
 alias spider='wget --spider -r -nd -nv -H -w 2'
-alias t='trash'
-alias cs='clocker start -t'
-alias ce='clocker stop'
-alias ca='clocker archive'
-alias cl='clocker list'
+alias dog='highlight -O ansi --force'
 alias ack='ack-grep'
 alias copy='xclip -sel clip <'
-alias selenium='java -jar ~/.selenium-server.jar'
 alias createCommitMsgHook='. ~/dotfiles/scripts/createCommitMsgHook.sh'
 alias serve='python -m SimpleHTTPServer'
-alias pt='perltidy'
 alias search='grep -nrw . -e'
 alias less='less -N'
 alias grs='git reset --soft HEAD~1'
@@ -39,15 +33,8 @@ command -v md5sum > /dev/null || alias md5sum="md5"
 # OS X has no `sha1sum`, so use `shasum` as a fallback
 command -v sha1sum > /dev/null || alias sha1sum="shasum"
 
-# Disable Spotlight
-alias spotoff="sudo mdutil -a -i off"
-# Enable Spotlight
-alias spoton="sudo mdutil -a -i on"
-
 # Reload the shell (i.e. invoke as a login shell)
 alias reload="exec $SHELL -l"
-
-alias homestead='function __homestead() { (cd ~/projects/tools/Homestead && vagrant $*); unset -f __homestead; }; __homestead'
 
 if [ -f ~/bin/aliases.zsh ]; then
     source ~/bin/aliases.zsh
