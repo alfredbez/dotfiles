5b6aa4cf1d95691be0733eb06dd9fd03a42d64a5
Alfred Bez <alfred.bez@googlemail.com>
2024-06-16 09:30:00 +0200

    Complete vim to neovim migration and cleanup
    - Remove old .vimrc and init.vim files
    - Update install.sh to symlink nvim directory instead of init.vim
    - Add new nvim configuration directory
    - Add screen-256color terminfo for better terminal support
    - Clean up legacy vim configuration files

---
+++ nvim/lua/plugins/colors.lua
@@ -0,0 +1,10 @@
+return {
+  -- { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
